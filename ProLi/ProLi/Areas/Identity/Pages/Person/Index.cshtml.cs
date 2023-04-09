965a425148b9de820c490f89b6684200b40a3c67
AndiHalasi <92200123+AndiHalasi@users.noreply.github.com>
2023-04-09 18:04:04 +0200

    SQL Servert csatoltam.

---
+++ ProLi/ProLi/Areas/Identity/Pages/Person/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ProLi.Areas.Identity.Pages.Person
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
