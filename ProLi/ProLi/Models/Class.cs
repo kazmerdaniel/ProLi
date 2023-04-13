16ac991c0b1d64b44d72a74eb6a8d3035f3cfbd2
kazmerdaniel <129666238+kazmerdaniel@users.noreply.github.com>
2023-04-13 23:23:12 +0200

    Felhasználók rögzítése2

---
+++ ProLi/ProLi/Models/Class.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ProLi.Models
+{
+    public class IdentityUserWithRole: IdentityUser 
+    {
+        public string UserRole { get; set; }
+        public string Password  { get; set; }   
+      
+    }
+}
---
+++ ProLi/ProLi/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace ProLi.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string UserRole { get; set; }
+ 
+    }
+}
