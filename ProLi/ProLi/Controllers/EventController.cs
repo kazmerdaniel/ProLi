25f81f09825ab698e530e18288e5d71fb744c554
AndiHalasi <92200123+AndiHalasi@users.noreply.github.com>
2023-05-03 19:22:09 +0200

    Meghívottak listájának exportálása excelbe.

--- ProLi/ProLi/Controllers/EventController.cs
+++ ProLi/ProLi/Controllers/EventController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Linq.Expressions;
 using System.Reflection;
+using System.Text;
 using ClosedXML.Excel;
 using DocumentFormat.OpenXml.Office2010.Excel;
 using Microsoft.AspNetCore.Authorization;
@@ -16,7 +17,7 @@ namespace ProLi.Controllers
     {
         private readonly ApplicationDbContext _context;
         private List<EventPeople> eventPeoples = new List<EventPeople>();     //mehívások
-     
+
 
         public EventController(ApplicationDbContext context)
         {
@@ -111,7 +112,7 @@ namespace ProLi.Controllers
             var @event = await _context.Event
             .Include(e => e.People)
                 .FirstOrDefaultAsync(e => e.Id == id);
-          
+
             if (@event == null)
             {
                 return NotFound();
@@ -131,11 +132,11 @@ namespace ProLi.Controllers
                 .FirstOrDefaultAsync(e => e.Id == id);
             var personToRemove = @event.People.Single((p) => p.Id == personId);
             @event.People.Remove(personToRemove);
-          
+
 
             if (@event != null)
             {
-                 _context.SaveChanges();
+                _context.SaveChanges();
 
             }
             return RedirectToAction("Details", new { id = id });
@@ -143,7 +144,7 @@ namespace ProLi.Controllers
         }
 
         // GET: People/Create
-        public async Task <IActionResult> Create()
+        public async Task<IActionResult> Create()
         {
             List<People> userList = _context.People.ToList();
             ViewBag.ShowMembers = new SelectList(userList, "Id", "GuestName");
@@ -164,14 +165,14 @@ namespace ProLi.Controllers
             {
                 ViewData["people"] = people;
             }
-            return View(filtered);            
-        } 
+            return View(filtered);
+        }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> AddToEvent(int? id, int person)
         {
-           
+
             // a kiválasztott személy hozzárendelése a kiválasztott adatbázishoz
             if (id == null || _context.Event == null)
             {
@@ -188,8 +189,8 @@ namespace ProLi.Controllers
                 await _context.SaveChangesAsync();
             }
 
-            return RedirectToAction(nameof(Index));       
-            
+            return RedirectToAction(nameof(Index));
+
         }
 
         // POST: People/Create
@@ -207,7 +208,7 @@ namespace ProLi.Controllers
         // GET: People/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
-           
+
             if (id == null || _context.Event == null)
             {
                 return NotFound();
@@ -233,27 +234,27 @@ namespace ProLi.Controllers
                 return NotFound();
             }
 
-            
-                try
-                {
-                    _context.Update(people);
-                    await _context.SaveChangesAsync();
+
+            try
+            {
+                _context.Update(people);
+                await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
 
-                }
-                catch (DbUpdateConcurrencyException)
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PeopleExists(people.Id))
                 {
-                    if (!PeopleExists(people.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                    throw;
-                    }
-
+                    return NotFound();
                 }
-            
+                else
+                {
+                    throw;
+                }
+
+            }
+
         }
         [Authorize(Roles = "System,Administrator")]
         // GET: People/Delete/5
@@ -288,51 +289,75 @@ namespace ProLi.Controllers
             {
                 _context.Event.Remove(people);
             }
-            
+
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
 
         private bool PeopleExists(int id)
         {
-          return (_context.Event?.Any(e => e.Id == id)).GetValueOrDefault();
+            return (_context.Event?.Any(e => e.Id == id)).GetValueOrDefault();
         }
 
 
 
-        //****************** Excel export **************************
+        //Adott rendezvény résztvevőinek listája exportálás Excelbe
+           
 
-        [HttpPost, ActionName("Export")]
-        public IActionResult ExportToExcel()
+        [HttpPost, ActionName("ExportToExcel1")]
+        public IActionResult ExportToExcel1(int id)
         {
             using (var workbook = new XLWorkbook())
             {
+                var eventDetails = _context.Event.Include(e => e.People).FirstOrDefault(e => e.Id == id);
+                if (eventDetails == null)
+                {
+                    return NotFound();
+                }
 
                 var worksheet = workbook.Worksheets.Add("Meghívottak");
-                var currentRow = 3;
-         
-        
-                worksheet.Cell(currentRow, 1).Value = "Id";
-                worksheet.Cell(currentRow, 2).Value = "Name";
-                              
+                var currentRow = 6;
 
-                foreach (var people in _context.People)
+                worksheet.Cell(1, 1).Value = eventDetails.EventName;
+                worksheet.Cell(1, 1).Style.Font.Bold = true;
+                worksheet.Cell(2, 1).Value = eventDetails.EventTime;
+                worksheet.Cell(2, 1).Style.Font.Bold = true;
+                worksheet.Cell(3, 1).Value = eventDetails.Place;
+                worksheet.Cell(3, 1).Style.Font.Bold = true;
+                worksheet.Cell(5, 1).Value = "Résztvevők:";
+                worksheet.Cell(5, 1).Style.Font.Bold = true;
+
+                worksheet.Range("A6:F6").Style.Fill.BackgroundColor = XLColor.LightGray;
+                worksheet.Range("A1:F4").Style.Font.FontSize = 12;
+
+                worksheet.Cell(currentRow, 1).Value = "Név";
+                worksheet.Cell(currentRow, 2).Value = "e-mail cím";
+                worksheet.Cell(currentRow, 3).Value = "Cím";
+                worksheet.Cell(currentRow, 4).Value = "Telefon";
+                worksheet.Cell(currentRow, 5).Value = "Ország";
+                worksheet.Cell(currentRow, 6).Value = "Megjegyzés";
+
+                foreach (var people in eventDetails.People)
                 {
                     currentRow++;
                     worksheet.Cell(currentRow, 1).Value = people.GuestName;
                     worksheet.Cell(currentRow, 2).Value = people.Email;
-
+                    worksheet.Cell(currentRow, 3).Value = people.Address;
+                    worksheet.Cell(currentRow, 4).Value = people.Phone;
+                    worksheet.Cell(currentRow, 5).Value = people.Country;
+                    worksheet.Cell(currentRow, 6).Value = people.Note;
                 }
+
+                worksheet.Range("A5:F" + currentRow).Style.Font.FontSize = 12;
+                worksheet.Range("A5:F5").Style.Font.Bold = true;
+
                 using (var stream = new MemoryStream())
                 {
                     workbook.SaveAs(stream);
                     var content = stream.ToArray();
-                    return File(content, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "protokoll lista.xlsx");
+                    return File(content, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", $"{eventDetails.EventName} résztvevők listája.xlsx");
                 }
             }
-
         }
-
-
-    }
+    }  
 }
