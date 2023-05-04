cef0a40835cc66f088f89a2758b377b6e0dbf8f4
AndiHalasi <92200123+AndiHalasi@users.noreply.github.com>
2023-05-04 20:06:57 +0200

    office adatok kötelező mezőire figyelmeztetés

--- ProLi/ProLi/Models/Office.cs
+++ ProLi/ProLi/Models/Office.cs
@@ -12,25 +12,46 @@ namespace ProLi.Models
         [Key]
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "A mező kitöltése kötelező")]
         [Display(Name = "Pozíció")]
         public string OfficePost { get; set; }
+
+        [Required(ErrorMessage = "A mező kitöltése kötelező")]
         [Display(Name = "Jogviszony kezdete")]
         public DateTime OfficeStart { get; set; }
+
+
         [Display(Name = "Jogviszony vége")]
         public DateTime? OfficeEnd { get; set; }
+
+
         [Display(Name = "Hivatal neve 1.szint")]
+        [Required(ErrorMessage = "Legalább egy hivatalt meg kell adni!")]
         public string OfficeName1 { get; set; }
+
+
         [Display(Name = "Hivatal neve 2.szint")]
         public string? OfficeName2 { get; set; }
+
+
         [Display(Name = "Hivatal neve 3.szint")]
         public string? OfficeName3 { get; set; }
+
+
         [Display(Name = "Cím")]
         public string? OfficeAddress { get; set; }
+
+
         [Display(Name = "E-mail")]
         public string? OfficeEmail { get; set; }
+
+
         [Display(Name = "Telefonszám")]
         public string? OfficePhone { get; set; }
+
+
         [Display(Name = "Azonosító kód")]
+        [Required(ErrorMessage = "A mező kitöltése kötelező")]
         public int People_Id { get; set; }
 
 
